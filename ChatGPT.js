563c0cd24d2772d0512d033cbb879d77a496fa7e
Nicholas Davies (Rickk#5520) <nicholas.john.davies42@gmail.com>
2023-03-22 19:34:49 +0800

    init

---
+++ ChatGPT.js
@@ -0,0 +1,20 @@
+// {
+//     'id': 'chatcmpl-6p9XYPYSTTRi0xEviKjjilqrWU2Ve',
+//     'object': 'chat.completion',
+//     'created': 1677649420,
+//     'model': 'gpt-3.5-turbo',
+//     'usage': {'prompt_tokens': 56, 'completion_tokens': 31, 'total_tokens': 87},
+//     'choices': [
+//       {
+//        'message': {
+//          'role': 'assistant',
+//          'content': 'The 2020 World Series was played in Arlington, Texas at the Globe Life Field, which was the new home stadium for the Texas Rangers.'},
+//        'finish_reason': 'stop',
+//        'index': 0
+//       }
+//      ]
+//    }
+// example res. ^^^^
+
+// applicationID = 1088061654459232336
+// public = 981e7105d6a96b185c5f7872475ebc0da2138771ff9cc7b76172187791772f0b
